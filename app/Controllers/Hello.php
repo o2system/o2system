689abd24e4299e4b0dfed694c559ba33a9da6121
Teguh Rianto <teguh.rianto22@gmail.com>
2019-03-30 21:44:41 +0700

    update vendor

--- app/Controllers/Hello.php
+++ app/Controllers/Hello.php
@@ -28,6 +28,7 @@ class Hello extends Controller
      */
     public function index()
     {
+        print_out('hello');
         presenter()->page->setHeader( 'Hello World' );
 
         view('hello');
