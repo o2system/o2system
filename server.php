34da7fee53258f81c956e9467c4b9a370e350c77
Steeven Andrian <steevenz@steevenz.com>
2019-05-02 23:38:43 +0700

    Update server.php

--- server.php
+++ server.php
@@ -11,14 +11,27 @@
 // ------------------------------------------------------------------------
 
 $uri = urldecode(
-    parse_url( $_SERVER[ 'REQUEST_URI' ], PHP_URL_PATH )
+    parse_url($_SERVER[ 'REQUEST_URI' ], PHP_URL_PATH)
 );
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
-if ( $uri !== '/' && file_exists( __DIR__ . '/public' . $uri ) ) {
+if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
     return false;
+} elseif ($uri !== '/' && file_exists($filePath = __DIR__ . str_replace('\\', '/', DIRECTORY_SEPARATOR))) {
+    if (strpos(dirname($filePath), 'database' !== false)) {
+        return false;
+    } elseif (strpos(dirname($filePath), 'cache' !== false)) {
+        return false;
+    } else {
+        header('Content-Disposition: filename=' . pathinfo($filePath, PATHINFO_BASENAME));
+        header('Content-Transfer-Encoding: binary');
+        header('Last-Modified: ' . gmdate('D, d M Y H:i:s', time()) . ' GMT');
+        header('Content-Type: ' . filemtime($filePath));
+        echo readfile($filePath);
+        exit(EXIT_SUCCESS);
+    }
 }
 
 require_once __DIR__ . '/public/index.php';
